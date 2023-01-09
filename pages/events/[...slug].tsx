365fabab8c4926c3de9bd41dc0924e4dc786da70
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-01-09 10:09:50 +0530

    update: search res page

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -2,10 +2,45 @@ import { Box, Container, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
 import EventList from "../../components/events/EventList";
+import { getFilteredEvents } from "../../dummy-data";
 
 function FilteredEventsPage() {
   const router = useRouter();
   const filterData = router.query.slug;
+   
+  const filteredYear = filterData && filterData[0];
+  const filteredMonth = filterData && filterData[1];
+
+  const numYear = +`${filteredYear}`;
+  const numMonth = +`${filteredMonth}`;
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Box>
+        <Text>No events found for the chosen filter</Text>
+      </Box>
+    );
+  }
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <Box>
+        <Text>Invalid Filter. Please adjust your values</Text>
+      </Box>
+    );
+  }
 
   if (!filterData) {
     return (
@@ -14,10 +49,11 @@ function FilteredEventsPage() {
       </Box>
     );
   }
+
   return (
     <Container>
       <Text>Filtered Events</Text>
-      <EventList items={filterData} />
+      <EventList items={filteredEvents} />
     </Container>
   );
 }
